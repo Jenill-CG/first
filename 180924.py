1b2d2a8c71960abc07357b6d2dcd683f7247ca06
Jenill-CG <jenil@convegenius.ai>
2024-09-20 14:10:06 +0530

    Update 180924.py

--- 180924.py
+++ 180924.py
@@ -433,7 +433,7 @@ def main():
     # Display the text and table
     st.markdown(css, unsafe_allow_html=True)
     # st.warning("""Please rename your column headers as per input file structure shown""")
-    st.markdown("Please modify your column headers to match the specified structure⬇️")
+    st.markdown("Please modify your column headers⬇️ to match the specified structure:")
     st.markdown(html_table, unsafe_allow_html=True)
     
     st.info(
