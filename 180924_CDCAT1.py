6d30fe530e1f4ae5ad9b105b460d70ac17c9069b
Jenill-CG <jenil@convegenius.ai>
2025-12-05 18:11:50 +0530

    Update 180924_CDCAT1.py

--- 180924_CDCAT1.py
+++ 180924_CDCAT1.py
@@ -198,8 +198,8 @@ def create_attendance_pdf(pdf, column_widths, column_names, image_path, info_val
                     break
 
     # Width for the school name and date of assessment cells
-    school_name_width = info_cell_width * 0.99  # 65% of the total width for the school name
-    # date_width = info_cell_width * 0.01         # 35% of the total width for the date of assessment
+    school_name_width = info_cell_width * 0.65  # 65% of the total width for the school name
+    date_width = info_cell_width * 0.35         # 35% of the total width for the date of assessment
     # school_name_width = info_cell_width   # 65% of the total width for the school name
     
     # Add the DISTRICT, BLOCK, and other labels
@@ -211,11 +211,11 @@ def create_attendance_pdf(pdf, column_widths, column_names, image_path, info_val
     pdf.cell(info_cell_width, 3, f"SCHOOL : {info_labels['SCHOOL']}", border='L', ln=0)  # Left border only
 
     # Set a different font for the DATE OF ASSESSMENT
-    # pdf.set_font('Arial', 'B', 4.5)  # Set to Arial, Italic, size 5
+    pdf.set_font('Arial', 'B', 4.5)  # Set to Arial, Italic, size 5
 
     # Add the DATE OF ASSESSMENT on the right side
     # pdf.cell(date_width, 3, "DATE OF ASSESSMENT : ______________                       ", border='R', ln=1, align='R')  # Right border only
-    # pdf.cell(date_width, 3, "DATE OF ASSESSMENT : ______________                       ", ln=1, align='R')  # Right border only
+    pdf.cell(date_width, 3, "DATE OF ASSESSMENT : ______________                       ", ln=1, align='R')  # Right border only
     
     
     # Reset the font back to the original for the remaining labels
