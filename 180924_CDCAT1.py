2835390b6670d8ac4c418a7b8e44fe0839401a79
Jenill-CG <jenil@convegenius.ai>
2025-12-05 17:57:23 +0530

    Update 180924_CDCAT1.py

--- 180924_CDCAT1.py
+++ 180924_CDCAT1.py
@@ -198,7 +198,7 @@ def create_attendance_pdf(pdf, column_widths, column_names, image_path, info_val
                     break
 
     # Width for the school name and date of assessment cells
-    # school_name_width = info_cell_width * 0.65  # 65% of the total width for the school name
+    school_name_width = info_cell_width * 0.65  # 65% of the total width for the school name
     # date_width = info_cell_width * 0.35         # 35% of the total width for the date of assessment
 
     # Add the DISTRICT, BLOCK, and other labels
