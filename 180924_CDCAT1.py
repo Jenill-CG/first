2c2f9c4f40f26e7ad06e739f9fff3e3bc3b3f6e4
Jenill-CG <jenil@convegenius.ai>
2025-12-05 19:15:28 +0530

    Update 180924_CDCAT1.py

--- 180924_CDCAT1.py
+++ 180924_CDCAT1.py
@@ -227,7 +227,7 @@ def create_attendance_pdf(pdf, column_widths, column_names, info_values, df, for
     pdf.cell(info_cell_width, 3, f"CLASS : {info_labels['CLASS']}", border='LR', ln=1)
     
     # Draw a border around the table header
-    pdf.set_font('Arial', 'B', 5)
+    pdf.set_font('Arial', 'B', 8)
     table_cell_height = 9
 
     # Add the Title and Subtitle in the Center
@@ -292,7 +292,7 @@ def create_attendance_pdf(pdf, column_widths, column_names, info_values, df, for
         pdf.ln(0.5)
 
         # Second row of headers (merged cells)
-        pdf.set_font("Arial",  size=5)
+        pdf.set_font("Arial",  size=7)
         pdf.cell(30, 4.5, '', border='LBR', align='C')  # Empty cell under STUDENT ID
         pdf.cell(87, 4.5, '', border='LBR', align='C')  # Empty cell under STUDENT NAME
         pdf.cell(18, 4.5, '(BOY/GIRL)', border='LBR', align='C')  # Empty cell under GENDER
@@ -305,7 +305,7 @@ def create_attendance_pdf(pdf, column_widths, column_names, info_values, df, for
 
 
     # Table Rows (based on student_count)
-    pdf.set_font('Arial', '', 6)
+    pdf.set_font('Arial', '', 8)
     student_count = info_values.get('student_count', 0)  # Use 0 if 'student_count' is missing or not found
 
     # Fill in the student IDs for the selected school code
