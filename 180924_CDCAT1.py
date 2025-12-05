ea450e9c09863c5ea11cea35e767ebeceb3fabf3
Jenill-CG <jenil@convegenius.ai>
2025-12-05 18:02:21 +0530

    Update 180924_CDCAT1.py

--- 180924_CDCAT1.py
+++ 180924_CDCAT1.py
@@ -208,13 +208,14 @@ def create_attendance_pdf(pdf, column_widths, column_names, image_path, info_val
     pdf.cell(info_cell_width, 3, f"DISTRICT : {info_labels['DISTRICT']}", border='LR', ln=1)
     
     # Add the SCHOOL NAME
-    pdf.cell(school_name_width, 3, f"SCHOOL : {info_labels['SCHOOL']}", border='L', ln=0)  # Left border only
+    # pdf.cell(school_name_width, 3, f"SCHOOL : {info_labels['SCHOOL']}", border='L', ln=0)  # Left border only
+    pdf.cell(info_cell_width, 3, f"SCHOOL : {info_labels['SCHOOL']}", border='L', ln=0)  # Left border only
 
     # Set a different font for the DATE OF ASSESSMENT
     pdf.set_font('Arial', 'B', 4.5)  # Set to Arial, Italic, size 5
 
     # Add the DATE OF ASSESSMENT on the right side
-    pdf.cell(date_width, 3, "DATE OF ASSESSMENT : ______________                       ", border='R', ln=1, align='R')  # Right border only
+    # pdf.cell(date_width, 3, "DATE OF ASSESSMENT : ______________                       ", border='R', ln=1, align='R')  # Right border only
 
     # Reset the font back to the original for the remaining labels
     pdf.set_font('Arial', 'B', 5)
