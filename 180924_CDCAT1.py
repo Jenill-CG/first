c8f3485e4b3fa8efbb54c47d95472ccd3ed2cd03
Jenill-CG <jenil@convegenius.ai>
2025-12-05 17:50:59 +0530

    Update 180924_CDCAT1.py

--- 180924_CDCAT1.py
+++ 180924_CDCAT1.py
@@ -749,7 +749,7 @@ def main():
                 'SECTION': 12
             }
         else:
-            column_names = ['S.NO', 'STUDENT ID', 'STUDENT NAME', 'GENDER', 'TAB ID', 'SUBJECT 1', 'SUBJECT 2', 'SECTION', 'SESSION']
+            column_names = ['STUDENT ID', 'STUDENT NAME', 'GENDER', 'TAB ID', 'SUBJECT 1', 'SUBJECT 2', 'SECTION', 'SESSION']
             column_widths = {
                 # 'S.NO': 6,
                 'STUDENT ID': 21,
