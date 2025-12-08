d545cbc20d2102f7fdf023aca3a59a807aa9f9f1
Jenill-CG <jenil@convegenius.ai>
2025-12-08 10:59:39 +0530

    Update 180924_CDCAT1.py

--- 180924_CDCAT1.py
+++ 180924_CDCAT1.py
@@ -204,12 +204,12 @@ def create_attendance_pdf(pdf, column_widths, column_names, info_values, df, for
     # school_name_width = info_cell_width   # 65% of the total width for the school name
     
     # Add the DISTRICT, BLOCK, and other labels
-    pdf.cell(info_cell_width, 3, f"STATE : {info_labels['STATE']}", border='LR', ln=1)
-    pdf.cell(info_cell_width, 3, f"DISTRICT : {info_labels['DISTRICT']}", border='LR', ln=1)
+    pdf.cell(info_cell_width, 3, f"STATE : {info_labels['STATE']}", border=0, ln=1)
+    pdf.cell(info_cell_width, 3, f"DISTRICT : {info_labels['DISTRICT']}", border=0, ln=1)
     
     # Add the SCHOOL NAME
     # pdf.cell(school_name_width, 3, f"SCHOOL : {info_labels['SCHOOL']}",border=0, ln=0)  # Left border only
-    pdf.cell(info_cell_width, 3, f"SCHOOL : {info_labels['SCHOOL']}", border='L', ln=0)  # Left border only
+    pdf.cell(info_cell_width, 3, f"SCHOOL : {info_labels['SCHOOL']}", border=0, ln=0)  # Left border only
 
     # Set a different font for the DATE OF ASSESSMENT
     pdf.set_font('Arial', 'B', 4.5)  # Set to Arial, Italic, size 5
@@ -224,8 +224,8 @@ def create_attendance_pdf(pdf, column_widths, column_names, info_values, df, for
 
     # Add the CLASS and SECTION labels
     
-    pdf.cell(info_cell_width, 3, f"UDISE CODE : {info_labels['UDISE CODE']}", border='LR', ln=1)
-    pdf.cell(info_cell_width, 3, f"CLASS : {info_labels['CLASS']}", border='LR', ln=1)
+    pdf.cell(info_cell_width, 3, f"UDISE CODE : {info_labels['UDISE CODE']}", border=0, ln=1)
+    pdf.cell(info_cell_width, 3, f"CLASS : {info_labels['CLASS']}", border=0, ln=1)
     
     # Draw a border around the table header
     pdf.set_font('Arial', 'B', 8)
