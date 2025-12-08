7919a21a29d82279ef030facbc0d8b3c516f836f
Jenill-CG <jenil@convegenius.ai>
2025-12-08 13:36:42 +0530

    Update 180924_CDCAT1.py

--- 180924_CDCAT1.py
+++ 180924_CDCAT1.py
@@ -252,7 +252,7 @@ def create_attendance_pdf(pdf, column_widths, column_names, info_values, df, for
         pdf.cell(23, 4, '', border='LTR', align='C', fill=True)
         # pdf.cell(12, 4, '', border='LTR', align='C')
         # pdf.cell(12, 4, '', border='LTR', align='C')  # End of the row
-        pdf.ln(4)
+        # pdf.ln(4)
         # First row of headers
         pdf.cell(26, 0.5, 'STUDENT ID', border='LR', align='C', fill=True)
         pdf.cell(73, 0.5, 'STUDENT FULL NAME', border='LR', align='C', fill=True)
@@ -264,7 +264,7 @@ def create_attendance_pdf(pdf, column_widths, column_names, info_values, df, for
         # pdf.cell(12, 0.5, 'SESSION', border='LR', align='C')  # End of the row
 
         # Move to the next line
-        # pdf.ln(0.5)
+        pdf.ln(0.5)
 
         # Second row of headers (merged cells)
         pdf.set_font("Arial",  size=6.5)
