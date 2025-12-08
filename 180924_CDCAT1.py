be3a478b8240f790e555a3904396aea012d9fd0e
Jenill-CG <jenil@convegenius.ai>
2025-12-08 12:24:22 +0530

    Update 180924_CDCAT1.py

--- 180924_CDCAT1.py
+++ 180924_CDCAT1.py
@@ -169,7 +169,7 @@ def create_attendance_pdf(pdf, column_widths, column_names, info_values, df, for
     info_cell_width = merged_cell_width  # Width same as the merged title cell
     info_cell_height = 17  # Adjust height as needed
     
-    pdf.cell(info_cell_width, info_cell_height, '', border='B', ln=1)
+    pdf.cell(info_cell_width, info_cell_height, '', border=0, ln=1)
     pdf.set_xy(pdf.get_x(), pdf.get_y() - info_cell_height +6)  # Move back to the top of the cell
 
     chosen_state = st.session_state.selected_state  
