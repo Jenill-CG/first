d01bd1d7c3d350a11e50db5f128c4872d038bc26
Jenill-CG <jenil@convegenius.ai>
2025-12-08 16:49:45 +0530

    Update 180924_CDCAT1.py

--- 180924_CDCAT1.py
+++ 180924_CDCAT1.py
@@ -272,17 +272,17 @@ def create_attendance_pdf(pdf, column_widths, column_names, info_values, df, for
         # pdf.cell(12, 4, '', border='LTR', align='C')  # End of the row
         pdf.ln(3)
         # First row of headers
-        pdf.cell(34, 2, 'STUDENT ID',  border='LR', align='C', fill=True)
-        pdf.cell(75, 2, 'STUDENT FULL NAME',  border='LR', align='C', fill=True)
-        pdf.cell(16, 2, 'GENDER', border='LR', align='C', fill=True)
-        pdf.cell(16, 2, 'SET CODE', border='LR', align='C', fill=True)
-        pdf.cell(19, 2, 'PART A', border='LR', align='C', fill=True)
-        pdf.cell(19, 2, 'PART B', border='LR', align='C', fill=True)
+        pdf.cell(34, 2.5, 'STUDENT ID',  border='LR', align='C', fill=True)
+        pdf.cell(75, 2.5, 'STUDENT FULL NAME',  border='LR', align='C', fill=True)
+        pdf.cell(16, 2.5, 'GENDER', border='LR', align='C', fill=True)
+        pdf.cell(16, 2.5, 'SET CODE', border='LR', align='C', fill=True)
+        pdf.cell(19, 2.5, 'PART A', border='LR', align='C', fill=True)
+        pdf.cell(19, 2.5, 'PART B', border='LR', align='C', fill=True)
         # pdf.cell(12, 0.5, 'SECTION', border='LR', align='C')
         # pdf.cell(12, 0.5, 'SESSION', border='LR', align='C')  # End of the row
 
         # Move to the next line
-        pdf.ln(2)
+        pdf.ln(2.5)
 
         # Second row of headers (merged cells)
         pdf.set_font("calibri",  size=6.5)
