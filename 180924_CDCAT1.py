398898b8a5f165b627a90af7e4a83fecc14e331e
Jenill-CG <jenil@convegenius.ai>
2025-12-08 14:45:10 +0530

    Update 180924_CDCAT1.py

--- 180924_CDCAT1.py
+++ 180924_CDCAT1.py
@@ -200,8 +200,8 @@ def create_attendance_pdf(pdf, column_widths, column_names, info_values, df, for
     pdf.cell(info_cell_width, 8, f"CLASS : {info_labels['CLASS']}", border=0, ln=1)
 
     pdf.set_font('Arial', 'I', 10)
-    pdf.cell(info_cell_width*0.50, 6,"           All details should be filled by the test administrator in", border=0, ln=0)
-    pdf.cell(info_cell_width*0.45, 6,"English  BLOCK LETTERS", border=0, ln=1)
+    pdf.cell(info_cell_width*0.50, 6,"        All details should be filled by the test administrator in", border=0, ln=0)
+    pdf.cell(info_cell_width*0.45, 6,"English  BLOCK LETTERS", border=0, ln=0)
     pdf.cell(info_cell_width*0.050, 6,"only", border=0, ln=1)
     
     pdf.cell(info_cell_width, 2, '', border=0, ln=1)    
