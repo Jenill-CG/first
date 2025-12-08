68013eea199b423d852f536eeda86ef2077535d7
Jenill-CG <jenil@convegenius.ai>
2025-12-08 14:32:08 +0530

    Update 180924_CDCAT1.py

--- 180924_CDCAT1.py
+++ 180924_CDCAT1.py
@@ -200,9 +200,9 @@ def create_attendance_pdf(pdf, column_widths, column_names, info_values, df, for
     pdf.cell(info_cell_width, 8, f"CLASS : {info_labels['CLASS']}", border=0, ln=1)
 
     pdf.set_font('Arial', 'I', 10)
-    pdf.cell(info_cell_width*0.50, 6,"           All details should be filled by the test administrator in ", border=0, ln=0)
+    pdf.cell(info_cell_width*0.50, 6,"           All details should be filled by the test administrator in                    only", border=0, ln=0)
     pdf.cell(info_cell_width*0.40, 6,"      English  BLOCK LETTERS", border=0, ln=0)
-    pdf.cell(info_cell_width*0.30, 6,"only", border=0, ln=1)
+    # pdf.cell(info_cell_width*0.50, 6,"only", border=0, ln=1)
     
     pdf.cell(info_cell_width, 2, '', border=0, ln=1)    
     # Draw a border around the table header
