10b32f69164ab2d65248e8da95fec31965d46c6f
Jenill-CG <jenil@convegenius.ai>
2025-12-08 19:19:55 +0530

    Update 180924_CDCAT2.py

--- 180924_CDCAT2.py
+++ 180924_CDCAT2.py
@@ -77,8 +77,8 @@ def process_data(uploaded_file, partner_id, buffer_percent, grade, district_digi
     data['Partner_ID'] = str(partner_id).zfill(len(str(partner_id)))  # Padding Partner_ID
     data['Grade'] = grade
     # Assign unique IDs for District, Block, and School, default to "00" for missing values
-    # data['School_udise'] = data['School_ID'].astype(str).str.zfill(12)
-    data['School_udise'] = data['School_ID']
+    data['School_udise'] = data['School_ID'].astype(str).str.zfill(11)
+    # data['School_udise'] = data['School_ID']
     data['District_ID'] = data['District'].apply(lambda x: str(data['District'].unique().tolist().index(x) + 1).zfill(district_digits) if x != "NA" else "0".zfill(district_digits))
     data['Block_ID'] = data['Block'].apply(lambda x: str(data['Block'].unique().tolist().index(x) + 1).zfill(block_digits) if x != "NA" else "0".zfill(block_digits))
     data['School_ID'] = data['School_ID'].apply(lambda x: str(data['School_ID'].unique().tolist().index(x) + 1).zfill(school_digits) if x != "NA" else "0".zfill(school_digits))
