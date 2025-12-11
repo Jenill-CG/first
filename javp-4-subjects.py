bd4919557cd8ba49b8fd5f54f621761f87afc04a
Jenill-CG <jenil@convegenius.ai>
2025-12-11 10:50:23 +0530

    Update javp-4-subjects.py

--- javp-4-subjects.py
+++ javp-4-subjects.py
@@ -606,7 +606,7 @@ def main():
                             uploaded_file,
                             partner_id,
                             buffer_percent,
-                            grade.zfill(2),
+                            grade,
                             district_digits,
                             block_digits,
                             school_digits,
