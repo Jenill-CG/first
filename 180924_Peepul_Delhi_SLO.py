3d1cd503d2b3da64042b2ecd5f25f2e291ab47ab
Jenill-CG <jenil@convegenius.ai>
2026-01-27 15:16:24 +0530

    Update 180924_Peepul_Delhi_SLO.py

--- 180924_Peepul_Delhi_SLO.py
+++ 180924_Peepul_Delhi_SLO.py
@@ -201,8 +201,8 @@ def create_attendance_pdf(pdf, column_widths, column_names, image_path, info_val
     date_width = info_cell_width * 0.35         # 35% of the total width for the date of assessment
 
     # Add the DISTRICT, BLOCK, and other labels
-    pdf.cell(info_cell_width, 3.75, f"ZONE : {info_labels['ZONE']}", border='LR', ln=0)
-    # pdf.cell(info_cell_width, 3, f"BLOCK : {info_labels['BLOCK']}", border='LR', ln=1)
+    pdf.cell(, 3.75, f"ZONE : {info_labels['ZONE']}", border='LR', ln=0)
+    # pdf.cell(school_name_width, 3, f"BLOCK : {info_labels['BLOCK']}", border='LR', ln=1)
     
     # Set a different font for the DATE OF ASSESSMENT
     pdf.set_font('Arial', 'B', 4.5)
