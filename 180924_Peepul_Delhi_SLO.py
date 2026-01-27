fd5618e10324f34515a97ceabf5e7f9ecb9c04da
Jenill-CG <jenil@convegenius.ai>
2026-01-27 15:15:17 +0530

    Update 180924_Peepul_Delhi_SLO.py

--- 180924_Peepul_Delhi_SLO.py
+++ 180924_Peepul_Delhi_SLO.py
@@ -193,23 +193,28 @@ def create_attendance_pdf(pdf, column_widths, column_names, image_path, info_val
                 if label[:4].lower() == key[:4].lower():
                     info_labels[label] = value
                     break
-
+    # Reset the font back to the original for the remaining labels
+    pdf.set_font('Arial', 'B', 5)
+    
     # Width for the school name and date of assessment cells
     school_name_width = info_cell_width * 0.65  # 65% of the total width for the school name
     date_width = info_cell_width * 0.35         # 35% of the total width for the date of assessment
 
     # Add the DISTRICT, BLOCK, and other labels
-    pdf.cell(info_cell_width, 3.75, f"ZONE : {info_labels['ZONE']}", border='LR', ln=1)
+    pdf.cell(info_cell_width, 3.75, f"ZONE : {info_labels['ZONE']}", border='LR', ln=0)
     # pdf.cell(info_cell_width, 3, f"BLOCK : {info_labels['BLOCK']}", border='LR', ln=1)
-
-    # Add the SCHOOL NAME
-    pdf.cell(school_name_width, 3.75, f"SCHOOL NAME : {info_labels['SCHOOL NAME']}", border='L', ln=0)  # Left border only
-
+    
     # Set a different font for the DATE OF ASSESSMENT
-    pdf.set_font('Arial', 'B', 4.5)  # Set to Arial, Italic, size 5
+    pdf.set_font('Arial', 'B', 4.5)
+    pdf.cell(date_width, 3.75, "DATE OF ASSESSMENT : ______________                       ", border='R', ln=1, align='R')  # Right border only    
 
-    # Add the DATE OF ASSESSMENT on the right side
-    pdf.cell(date_width, 3.75, "DATE OF ASSESSMENT : ______________                       ", border='R', ln=1, align='R')  # Right border only
+    # Reset the font back to the original for the remaining labels
+    pdf.set_font('Arial', 'B', 5)    
+    
+    # Add the SCHOOL NAME
+    pdf.cell(info_cell_width, 3.75, f"SCHOOL NAME : {info_labels['SCHOOL NAME']}", border='L', ln=1)  # Left border only
+
+
 
     # Reset the font back to the original for the remaining labels
     pdf.set_font('Arial', 'B', 5)
